1e278d418419a6b1c8ce2c6f6ab514d26650f68a
hendisantika <hendisantika@yahoo.co.id>
2023-02-27 06:50:07 +0700

    #12 - Add flyway migration script ✏️ 📗 🐳

---
+++ src/main/resources/db/migration/V1_27022022_0650__Create_Shop_Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE shop
+(
+    id         integer NOT NULL auto_increment,
+    address    varchar(255),
+    country    varchar(255),
+    owner_name varchar(255),
+    shop_no    bigint  NOT NULL,
+    shop_type  varchar(255),
+    PRIMARY KEY (id)
+) engine=InnoDB
