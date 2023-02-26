bd2580ce161a360d1637c891cc9f400e1845fe93
hendisantika <hendisantika@yahoo.co.id>
2023-02-27 06:44:52 +0700

    #10 - Add home in ShopController ✏️ 📗 🐳

--- src/main/java/com/hendisantika/searchthymeleaf/controller/ShopController.java
+++ src/main/java/com/hendisantika/searchthymeleaf/controller/ShopController.java
@@ -1,8 +1,13 @@
 package com.hendisantika.searchthymeleaf.controller;
 
+import com.hendisantika.searchthymeleaf.model.Shop;
 import com.hendisantika.searchthymeleaf.service.ShopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +23,16 @@ import org.springframework.stereotype.Controller;
 @RequiredArgsConstructor
 public class ShopController {
     private final ShopService shopService;
+
+    @RequestMapping(path = {"/", "/search"})
+    public String home(Shop shop, Model model, String keyword) {
+        if (keyword != null) {
+            List<Shop> list = shopService.getByKeyword(keyword);
+            model.addAttribute("list", list);
+        } else {
+            List<Shop> list = shopService.getAllShops();
+            model.addAttribute("list", list);
+        }
+        return "index";
+    }
 }
