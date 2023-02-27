75954671b88ce7bd85d42cb05325c6f5bf9d46f7
hendisantika <hendisantika@yahoo.co.id>
2023-02-27 07:53:03 +0700

    #14 - Add Init Shop Data ✏️ 📗 🐳

---
+++ src/main/resources/db/migration/V2_27022022_0750__Init_Shop_Data.sql
@@ -0,0 +1,10 @@
+INSERT INTO searchDB.shop (id, address, country, owner_name, shop_no, shop_type)
+VALUES (1, 'Konoha 1', 'Indonesia', 'Naruto', 1, 'Mobile Shop');
+INSERT INTO searchDB.shop (id, address, country, owner_name, shop_no, shop_type)
+VALUES (2, 'Konoha 2', 'Korea', 'Jung Jung Keun', 2, 'Bakery Shop');
+INSERT INTO searchDB.shop (id, address, country, owner_name, shop_no, shop_type)
+VALUES (3, 'Konoha 3', 'India', 'Merege Hese', 3, 'Sari Shop');
+INSERT INTO searchDB.shop (id, address, country, owner_name, shop_no, shop_type)
+VALUES (4, 'Konoha 4', 'Japan', 'Gurame Kari Mastakane', 4, 'Drug Store');
+INSERT INTO searchDB.shop (id, address, country, owner_name, shop_no, shop_type)
+VALUES (5, 'Konoha 5', 'Konoha 1', 'Tissue Paseo', 5, 'Furniture');
